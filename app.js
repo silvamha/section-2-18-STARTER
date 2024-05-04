ebc7b4cfda7063e128a152e9b398e3fa609cead6
Ed <silva.email@yahoo.com>
2024-05-04 06:22:38 -0500

    feat: Add textClasses computed property for dynamic styling in app.js and index.html

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ let vm = Vue.createApp({
   data() {
     return {
       isPurple: false,
-      selectedColor:'',
-      size:150,
+      selectedColor: "",
+      size: 150,
+      transform_text:"rotate(30deg)"
     };
   },
   computed: {
     circle_classes() {
       return { purple: this.isPurple };
+    },
+    textClasses() {
+      return {
+        width: this.size + "px",
+        height: this.size + "px",
+        lineHeight: this.size + "px",
+      };
     },
   },
 }).mount("#app");
